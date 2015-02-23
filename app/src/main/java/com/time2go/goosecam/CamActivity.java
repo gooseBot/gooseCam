a65777332757db09bbbbf825ffd5866e1991bb80
gooseBot <time2goup@gmail.com>
2015-02-22 18:18:37 -0800

    basic working version
    using camera takepicture API.  Receives UUID from GG via UDP broadcast.
    Saves image with session UID to app folder.  This folder is synced via
    dropsync for Android.

--- app/src/main/java/com/time2go/goosecam/CamActivity.java
+++ app/src/main/java/com/time2go/goosecam/CamActivity.java
@@ -1,13 +1,30 @@
 package com.time2go.goosecam;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 public class CamActivity extends Activity {
 
@@ -16,6 +33,12 @@ public class CamActivity extends Activity {
     private TextView textMessage;
     private RunServerInThread runServer = null;
     private Handler mHandler = new Handler();
+    public final static String DEBUG_TAG = "CamActivity";
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    private static String mSessionID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +56,20 @@ public class CamActivity extends Activity {
         if (runServer != null) {
             runServer.stopMe();
         }
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Create an instance of Camera
+        if (mCamera==null) {
+            mCamera = getCameraInstance();
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        }
     }
 
     @Override
@@ -40,6 +77,16 @@ public class CamActivity extends Activity {
         super.onStart();  // Always call the superclass method first
         if (runServer != null) {
             runServer.startMe();
+        }
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mPreview.getHolder().removeCallback(mPreview);
+            mCamera.stopPreview();
+            mCamera.setPreviewCallback(null);
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
         }
     }
 
@@ -62,11 +109,14 @@ public class CamActivity extends Activity {
                         public void run() {
                             // This gets executed on the UI thread so it can safely modify Views
                             textMessage.setText(message);
+                            capturePicture(message);
                         }
                     });
                 } catch (SocketException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
                     if (socket != null) {
@@ -86,4 +136,79 @@ public class CamActivity extends Activity {
             keepRunning = true;
         }
     }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+            Camera.Parameters params = c.getParameters();
+            params.setFocusMode(Camera.Parameters.FOCUS_MODE_INFINITY);
+            List<Camera.Size> sizes = params.getSupportedPictureSizes();
+            //looks like highest resolution is the first element.
+            params.setPictureSize(sizes.get(0).width, sizes.get(0).height);
+            c.setParameters(params);
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(DEBUG_TAG, "Error creating media file, check storage permissions: ");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+                Log.d(DEBUG_TAG, "Wrote file: " + pictureFile.toString());
+            } catch (FileNotFoundException e) {
+                Log.d(DEBUG_TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(DEBUG_TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+    public void capturePicture(String sessionUID) {
+        mSessionID=sessionUID.replaceAll("[^\\d.]", "");
+        mCamera.takePicture(null, null, mPicture);
+        mCamera.startPreview();
+    }
+
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+  mSessionID + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
 }
